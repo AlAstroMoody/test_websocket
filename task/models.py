3c272361e80389b8bcc7e62177a33069ba541fb3
Alexey Astrotenko <aastrotenko@mail.ru>
2019-11-10 21:11:18 +0700

    Добавлена таблица для сохранения данных с обработанных сайтов

--- task/models.py
+++ task/models.py
@@ -1,10 +1,13 @@
+from datetime import timedelta
+
 from django.db import models
 
 
 # работа со временем DurationField?
-# добавить в модель поля под: значения из парсера
+
 class TaskModel(models.Model):
     input_url = models.CharField(max_length=50, verbose_name = "Введите Url")
+
     objects = models.Manager()
 
     def __str__(self):
@@ -13,3 +16,10 @@ class TaskModel(models.Model):
     class Meta:
         verbose_name = 'Ввод url-ов'
         verbose_name_plural = 'Ввод url-ов'
+
+class OutputModel(models.Model):
+    save_url = models.CharField(max_length=50)
+    save_title = models.CharField(max_length=50)
+    save_h1 = models.CharField(max_length=50)
+    save_encoding = models.CharField(max_length=50)
+    objects = models.Manager()
--- task/parser.py
+++ task/parser.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+from .models import OutputModel
 
 # добавить проверку, существует ли сайт
 def parser(urls_name):
@@ -10,18 +10,39 @@ def parser(urls_name):
     data = []
 
     for link in links:
-        r = requests.get(link)
-        soup = BeautifulSoup(r.text, 'lxml')
-        try:
-            h1 = soup.find('h1').text.strip()
-        except:
-            h1 = " "
+        if OutputModel.objects.filter(save_url=link).exists():
+            j = ''
+            result = OutputModel.objects.filter(save_url=link).values('save_url', 'save_title', 'save_h1', 'save_encoding')
+            list_result = [entry for entry in result]
+            for i in list_result:   # хитромудрый способ вытащить данные из queryset
+                k = i.values()
+                e = ''
+                r = 0
+                for q in k:
+                    r += 1
+                    if r ==1:
+                        e += str(q) + ' — '
+                    else:
+                        e += str(q) + '  '
+                j += str(e)
+            data.append(j)
 
-        try:
-            title = soup.find('title').text.strip()
-        except:
-            title = " "
-        header = r.headers['Content-Type']
-        data.append((link + " — " + title + ' | ' + h1  + ' | ' + header))
+        else:
+            r = requests.get(link)
+            soup = BeautifulSoup(r.text, 'lxml')
+            try:
+                h1 = soup.find('h1').text.strip()
+            except:
+                h1 = " "
+
+            try:
+                title = soup.find('title').text.strip()
+            except:
+                title = " "
+            header = r.headers['Content-Type']
+            data.append((link + " — " + title + ' | ' + h1 + ' | ' + header))
+
+            b = OutputModel(save_url=link, save_title=title, save_h1=h1, save_encoding=header)
+            b.save()
 
     return data
