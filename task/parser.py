fbac132ab92181ec4998f0f73c7383a643af4c4f
Alexey Astrotenko <aastrotenko@mail.ru>
2019-11-10 21:25:30 +0700

    добавлена проверка на доступность сайта

--- task/parser.py
+++ task/parser.py
@@ -10,39 +10,42 @@ def parser(urls_name):
     data = []
 
     for link in links:
-        if OutputModel.objects.filter(save_url=link).exists():
-            j = ''
-            result = OutputModel.objects.filter(save_url=link).values('save_url', 'save_title', 'save_h1', 'save_encoding')
-            list_result = [entry for entry in result]
-            for i in list_result:   # хитромудрый способ вытащить данные из queryset
-                k = i.values()
-                e = ''
-                r = 0
-                for q in k:
-                    r += 1
-                    if r ==1:
-                        e += str(q) + ' — '
-                    else:
-                        e += str(q) + '  '
-                j += str(e)
-            data.append(j)
+        try:
+            if OutputModel.objects.filter(save_url=link).exists():
+                j = ''
+                result = OutputModel.objects.filter(save_url=link).values('save_url', 'save_title', 'save_h1', 'save_encoding')
+                list_result = [entry for entry in result]
+                for i in list_result:   # хитромудрый способ вытащить данные из queryset
+                    k = i.values()
+                    e = ''
+                    r = 0
+                    for q in k:
+                        r += 1
+                        if r ==1:
+                            e += str(q) + ' — '
+                        else:
+                            e += str(q) + '  '
+                    j += str(e)
+                data.append(j)
 
-        else:
-            r = requests.get(link)
-            soup = BeautifulSoup(r.text, 'lxml')
-            try:
-                h1 = soup.find('h1').text.strip()
-            except:
-                h1 = " "
+            else:
+                r = requests.get(link)
+                soup = BeautifulSoup(r.text, 'lxml')
+                try:
+                    h1 = soup.find('h1').text.strip()
+                except:
+                    h1 = " "
 
-            try:
-                title = soup.find('title').text.strip()
-            except:
-                title = " "
-            header = r.headers['Content-Type']
-            data.append((link + " — " + title + ' | ' + h1 + ' | ' + header))
+                try:
+                    title = soup.find('title').text.strip()
+                except:
+                    title = " "
+                header = r.headers['Content-Type']
+                data.append((link + " — " + title + ' | ' + h1 + ' | ' + header))
 
-            b = OutputModel(save_url=link, save_title=title, save_h1=h1, save_encoding=header)
-            b.save()
+                b = OutputModel(save_url=link, save_title=title, save_h1=h1, save_encoding=header)
+                b.save()
+        except:
+            data.append(link + ' - ошибочный url')
 
     return data
