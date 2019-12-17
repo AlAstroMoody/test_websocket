d7ab0cf08e81bd3c3cfe4b61d16a4a8e050ff31f
Alexey Astrotenko <aastrotenko@mail.ru>
2019-12-17 12:34:24 +0700

    заработало внезапно

--- my_server.py
+++ my_server.py
@@ -1,29 +1,30 @@
 from datetime import datetime
 import asyncio
 import websockets
+import os
+import django
 
-# не работает импорт
-# from .models import TaskModel
-# from .new_parser import parser
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "challenge.settings")
+django.setup() 
+
+from task.models import TaskModel
+from task.new_parser import parser
 
 
 async def server(websocket, path):
-    # for i in time_records:
-    #     lst_timer.append(int(i.get('input_seconds')) + int(i.get('input_minutes')) * 60)
-    # count = 0
+    for i in time_records:
+        lst_timer.append(int(i.get('input_seconds')) + int(i.get('input_minutes')) * 60)
+    count = 0
     while True:
-        timer = datetime.now().strftime("%d/%m/%y %H:%M:%S")
-        # await asyncio.sleep(lst_timer[count])
-        # solution = parser(all_records[count].get('input_url'))
-        #await websocket.send(solution)
-        await websocket.send(timer)
-        await asyncio.sleep(1)
-        # count += 1
+        await asyncio.sleep(lst_timer[count])
+        solution = parser(all_records[count].get('input_url'))
+        await websocket.send(solution)
+        count += 1
 
 
 start_server = websockets.serve(server, "127.0.0.1", 5680)
 lst_timer = []
-# all_records = TaskModel.objects.values('input_url')
-# time_records = TaskModel.objects.values('input_seconds', 'input_minutes')
+all_records = TaskModel.objects.values('input_url')
+time_records = TaskModel.objects.values('input_seconds', 'input_minutes')
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
